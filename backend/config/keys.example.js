cb75bee710dd04872e36fd2733d88f72842361ca
Maite <maitecondepalacio@gmail.com>
2020-04-08 13:06:58 +0200

    register added

---
+++ backend/config/keys.example.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://<name>:<password>@clusterbta-tuqrv.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ backend/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const {MONGO_URI} =require('../backend/config/keys')
+// const flash = require('connect-flash');
+// const session = require('express-session');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, { useUnifiedTopology: true ,useNewUrlParser: true})
+.then(()=>console.log('conectado a mongoDB con éxito'))
+.catch(err=>console.error(err))
+
+
+// Express body parser
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+
+// Express session
+// app.use(
+//   session({
+//     secret: 'secret',
+//     resave: true,
+//     saveUninitialized: true
+//   })
+// );
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+// app.use(flash());
+
+// Global variables
+// app.use(function(req, res, next) {
+//   res.locals.success_msg = req.flash('success_msg');
+//   res.locals.error_msg = req.flash('error_msg');
+//   res.locals.error = req.flash('error');
+//   next();
+// });
+
+// Routes
+
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
