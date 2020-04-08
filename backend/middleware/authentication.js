19a7c7cbe897210324228b4b85798f1560e85d4a
Sofía Pinilla <contact@sofiapinilla.com>
2020-04-08 15:38:22 +0200

    added login

---
+++ backend/middleware/authentication.js
@@ -0,0 +1,19 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const authentication = async(req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        const payload = jwt.verify(token, 'secretitos');
+        const user = await User.findByPk(payload.id);
+        if (!user || !tokenFound) {
+            res.status(401).send({ message: 'No estas autorizado' });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ error, message: 'Ha habido un problema con el token' })
+    }
+
+}
+module.exports = { authentication }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const UserController = require('../controllers/UserController');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
+const { authentication } = require('../middleware/authentication');
 // Load User model
-const User = require('../models/User');
+
 // const { forwardAuthenticated } = require('../config/auth');
 
 // Login Page
@@ -13,88 +13,18 @@ router.get('/login', (req, res) => res.send('login'));
 router.get('/register', (req, res) => res.send('register'));
 
 // Register
-router.post('/register', (req, res) => {
-        const { name, email, password, password2 } = req.body;
-        let errors = [];
-
-        if (!name || !email || !password || !password2) {
-
-            errors.push({ msg: 'Please enter all fields' });
-        }
-        if (password != password2) {
-            errors.push({ msg: 'Passwords do not match' });
-        }
-        if (errors.length > 0) {
-            res.send({
-                errors,
-                name,
-                email,
-                password,
-                password2
-            })
-        } else {
-            newUser = new User({...req.body })
-            bcrypt.genSalt(10, (errors, salt) => {
-                bcrypt.hash(newUser.password, salt, (errors, hash) => {
-                    if (errors) throw errors;
-                    newUser.password = hash;
-                    newUser
-                        .save()
-                        .then(user => res.send(user))
-                        .catch(errors => console.log(errors));
-                });
-            })
-        }
-
-    })
-    // ('/register', (req, res) => {
-    //   const { name, email, password, password2 } = req.body;
-    //   
+router.post('/register', UserController.register);
 
 
-
-
-
-// if (password.length < 6) {
-//   errors.push({ msg: 'Password must be at least 6 characters' });
-// }
-
-
-// } else {
-//   User.findOne({ email: email }).then(user => {
-//     if (user) {
-//       errors.push({ msg: 'Email already exists' });
-//       res.send('register', {
-//         errors,
-//         name,
-//         email,
-//         password,
-//         password2
-//       });
-//     } else {
-//       const newUser = new User({
-//         name,
-//         email,
-//         password
-//       });
-
-//      
-// });
 
 // Login
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local', {
-        successRedirect: '/dashboard',
-        failureRedirect: '/users/login',
-        failureFlash: true
-    })(req, res, next);
-});
+router.post('/login', UserController.login);
 
 // Logout
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success_msg', 'You are logged out');
-    res.redirect('/users/login');
-});
+// router.get('/logout', (req, res) => {
+//     req.logout();
+//     req.flash('success_msg', 'You are logged out');
+//     res.redirect('/users/login');
+// });
 
 module.exports = router;
