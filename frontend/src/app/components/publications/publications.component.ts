070d28f00615e0439c6a443ee25766e27f5d6aaa
Sofía Pinilla <contact@sofiapinilla.com>
2020-04-13 11:45:06 +0200

    added date

--- frontend/src/app/components/publications/publications.component.ts
+++ frontend/src/app/components/publications/publications.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PublicationsService } from 'src/app/services/publications.service';
 import { UserService } from 'src/app/services/user.service';
 import { HttpResponse } from '@angular/common/http';
-
+import * as moment from "moment";
 @Component({
   selector: 'app-publications',
   templateUrl: './publications.component.html',
@@ -15,22 +15,36 @@ export class PublicationsComponent implements OnInit {
   public publications;
   constructor(public publicationsService: PublicationsService, public userService: UserService) { }
   ngOnInit(): void {
-
-    this.publicationsService.getAll()
-      .subscribe(
-        res => {
-          this.publicationsService.publications = res;
-        },
-        error => console.error(error)
-      );
+    this.getAllPublications();
+    setInterval(this.getAllPublications,20000);
   }
+  getAllPublications=()=>{
+    this.publicationsService.getAll()
+    .subscribe(
+      res => {
+        this.publicationsService.publications = res.map(this.getHaceCuanto);
+        console.log(this.userService.user)
+      },
+      error => console.error(error)
+    );
+  }
+  getHaceCuanto = publication => {
+    const creationDate = moment(publication.createdAt);
+    const diffWeeks = moment().diff(creationDate, 'weeks') ? moment().diff(creationDate, 'weeks') + ' weeks ago':'';
+    const diffDays = moment().diff(creationDate, 'days') ? moment().diff(creationDate, 'days') + ' days ago':'';
+    const diffHours = moment().diff(creationDate, 'hours') ? moment().diff(creationDate, 'hours') + ' hours ago':'';
+    const diffMinutes = moment().diff(creationDate, 'minutes') ? moment().diff(creationDate, 'minutes') + ' minutes ago':'';
+    const diffSeconds = moment().diff(creationDate, 'seconds') ? moment().diff(creationDate, 'seconds') + ' seconds ago':'';
 
+    publication['haceCuanto'] = diffWeeks || diffDays || diffHours || diffMinutes || diffSeconds;
+    return publication;
+  }
   deletePublic(publication) {
     const id = publication._id
     this.publicationsService.deleteOne(id)
       .subscribe(publication => {
         this.publicationsService.getAll()
-        .subscribe(res => {
+          .subscribe(res => {
             console.log(res)
             this.publicationsService.publications = res
           },
